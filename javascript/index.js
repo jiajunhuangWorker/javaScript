15dd7f7e54893dc3372d777497d0bba86505face
lucas <13531314036@163.com>
2019-12-04 07:53:33 +0800

    github

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,27 @@
-alert(11)
+/*
+变量声明 然后赋值
+*/
+var a=100;
+/*
+	1.变量名必须以英文开头
+	2.关键字不能使用
+	3.保留字不能使用
+*/ 
+
+/*
+原始值
+Number
+Boolean
+String
+undefined
+null
+*/
+
+/*
+引用值
+new array()
+new object()
+function 
+date
+RegExp
+*/
