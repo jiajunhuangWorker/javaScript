24b4fb3cba025b61c622ce86efb3dd6292cf07fc
lucas <13531314036@163.com>
2019-12-31 08:39:03 +0800

    pages/operator.html

---
+++ javascript/operator.js
@@ -0,0 +1,15 @@
+var a=1;
+/*
+先打印后++ 
+*/
+document.write(a ++)
+document.write(a);
+
+var b=1
+var c=b++ +1
+document.write('<br/>c:',c);
+document.write('<br/>b:',b);
+
+document.write('<br/>0%5:',0%5)
+
+console.info('---js比较运算符---')
