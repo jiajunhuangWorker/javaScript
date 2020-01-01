ebb108fef3560930a4d0d6bfd5349de90572549b
lucas <13531314036@163.com>
2020-01-02 07:41:30 +0800

    计算

--- javascript/operator.js
+++ javascript/operator.js
@@ -21,3 +21,98 @@ b=a-b;
 a=a-b;
 document.write('<br/>a',a);
 document.write('<br/>b',b);
+
+
+/*ascll码比较*/
+document.write('<br/>10>8:','10'>'8',' //两个字符串比较先查找到1然后1和8进行比较');
+document.write('<br/>Infinity == Infinity:',Infinity == Infinity);
+document.write('<br/>NaN == NaN:',NaN == NaN);
+
+/*逻辑运算符
+&&运算符先看前面的表达式转换为布尔值时否为真如果为真就看第二个表达式转换为布尔值的结果,如果只有两个
+表达式的化,只看第二个表达式,就返回该表达式的值
+
+undefined,null,NaN,"",0,false 转换为布尔值都为false
+*/
+/*
+&&
+*/
+document.write('<br/>1 && 3+2:',(1 && 3+2));
+document.write('<br/>0 && 3+2:',(0 && 3+2));
+document.write('<br/>1 && undefined:',(1 && undefined));
+document.write('<br/>0 && document.write("a"):',0 && document.write("a"));
+/*
+||
+兼容的时候使用
+ var e=e||window.event
+*/
+document.write('<br/>0||false||1:',0||false||1);
+/*
+！
+*/
+document.write('<br/>123',!123)
+document.write('<br/>')
+/*
+条件语句
+*/
+// var score = parseInt(window.prompt('textarea'));
+if(score>90 && score<=100){
+	document.write('alibaba');
+}else if(score>80 && score<=90){
+	document.write('tencent');
+}else if(score>70 && score<=80){
+	document.write('baidu');
+}else if(score>60 && score<=70){
+	document.write('mogujie');
+}else{
+	document.write('no,no,no');
+}
+
+/*
+循环语句
+*/
+var i=1;
+for(;i;){
+	document.write('<br/>i:'+i);
+	i++;
+	if(i>10){
+		i=0;
+	}
+}
+for (var i = 0; i <= 100; i++) {
+	if(i%3==0 && i%5==0 || i%7==0){
+		document.write('<br/>i能被3&&5||7整除:',i)
+	}
+}
+/*
+1.计算2的n次幂，n可输入n为自然数 
+2.计算n的阶乘,n可输 5*4*3*2*1
+3.斐波那 1 1 2 3 5 8
+4.编写一个三维输的正整数反向
+5.a,b,c三个数字大印出来
+6.打印出100以内的质数
+*/
+/*
+1.计算2的n次幂，n可输入n为自然数 
+*/
+var score = parseInt(window.prompt('请输要求2的多少次幂'));
+if(score!=0 ){
+	var num=1;
+	for(var i=0;i<score;i++){
+		num*=2;	
+	}
+	document.write('<br/>2的'+score+'次幂是:',num);
+}else{
+	document.write('<br/>2的0次幂是:1');
+}
+
+/*
+2.计算n的阶乘,n可输 5*4*3*2*1
+*/
+var score = parseInt(window.prompt('请输阶乘数字'));
+var str='',result=1;
+for(var i=score;i>0;i--){
+	str+=i>1?i+'*':i;
+	result*=i;
+}
+document.write('<br/>'+str+':'+result);
