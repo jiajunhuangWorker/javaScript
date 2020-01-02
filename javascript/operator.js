510d1e3c0d59f98a78b957cc63b492248fba1f66
hjj <1459638155@qq.com>
2020-01-02 17:59:44 +0800

    object

--- javascript/operator.js
+++ javascript/operator.js
@@ -55,7 +55,7 @@ document.write('<br/>')
 /*
 条件语句
 */
-// var score = parseInt(window.prompt('textarea'));
+var score = parseInt(window.prompt('textarea'));
 if(score>90 && score<=100){
 	document.write('alibaba');
 }else if(score>80 && score<=90){
@@ -85,7 +85,7 @@ for (var i = 0; i <= 100; i++) {
 	}
 }
 /*
-1.计算2的n次幂，n可输入n为自然数 
+1.计算2的n次幂，n可输入n为自然数
 2.计算n的阶乘,n可输 5*4*3*2*1
 3.斐波那 1 1 2 3 5 8
 4.编写一个三维输的正整数反向
@@ -93,13 +93,13 @@ for (var i = 0; i <= 100; i++) {
 6.打印出100以内的质数
 */
 /*
-1.计算2的n次幂，n可输入n为自然数 
+1.计算2的n次幂，n可输入n为自然数
 */
 var score = parseInt(window.prompt('请输要求2的多少次幂'));
-if(score!=0 ){
+if(score!=0){
 	var num=1;
 	for(var i=0;i<score;i++){
-		num*=2;	
+		num*=2;
 	}
 	document.write('<br/>2的'+score+'次幂是:',num);
 }else{
@@ -109,10 +109,106 @@ if(score!=0 ){
 /*
 2.计算n的阶乘,n可输 5*4*3*2*1
 */
-var score = parseInt(window.prompt('请输阶乘数字'));
+var score2 = parseInt(window.prompt('请输阶乘数字'));
 var str='',result=1;
-for(var i=score;i>0;i--){
+for(var i=score2;i>0;i--){
 	str+=i>1?i+'*':i;
 	result*=i;
 }
 document.write('<br/>'+str+':'+result);
+
+/*3.斐波那
+斐波那契数列（意大利语：Successione di Fibonacci），又译为菲波拿契数列、菲波那西数列、斐氏数列、黄金分割数列。
+1 1 2 3 5 8
+*/
+var score3 = parseInt(window.prompt('请输入斐波那数列'));
+function num(n){
+	n--;
+	if(1<n){
+		num(n);
+	}
+	console.info(n+'+'+(n-1)+':'+(n+n-1))
+}
+/*
+4.编写一个三位输的正整数反向
+*/
+var score4 = window.prompt('请输入3位数字');
+var result='';
+for(i=score4.length;0<i;i--){
+	result+=score4[i-1];
+}
+document.write('<br/>'+score4+'反转之后:'+result);
+/*
+5.a,b,c三个数字比较
+*/
+
+/*
+6.打印出100以内的质数
+*/
+var score6 = window.prompt('请输入数字检验出质数');
+var str='';
+for(var i=score6;1<i;i--){
+	if(i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0){
+		str+=i+'-';
+	}
+}
+document.write('<br/>'+score6+'的质数有:'+str+'7-5-3-2');
+
+
+
+
+/*
+typeof number,string,boolean,object,undefined, function
+*/
+console.info('typeof null:',typeof null);
+console.info('typeof 1:',typeof 1);
+console.info('typeof 你好:',typeof '你好');
+console.info('typeof false:',typeof false);
+console.info('typeof []:',typeof []);
+console.info('typeof {}:',typeof {});
+console.info('typeof undefined:',typeof undefined);
+console.info('typeof function (){}:',typeof function (){});
+
+/*
+	显式转换
+*/
+var num = Number('123')
+console.info("Number('123')", num);
+var num = Number('undefined')
+console.info("Number(undefined)", num);
+var num = Number(true)
+console.info("Number(true)", num);
+var num = Number(false)
+console.info("Number(falae)", num);
+var num = Number('a')
+console.info("Number('a')", num);
+console.info('--------parseInt---------')
+var num = parseInt('a');
+console.info("parseInt('a')", num);
+var num = parseInt('a123');
+console.info("parseInt('a123')",num);
+var num = parseInt('123a');
+console.info("parseInt('123a')",num);
+var num = parseInt('123.9');
+console.info("parseInt('123.9')",num);
+var num = parseInt(true);
+console.info("parseInt(true)",num);
+var num = parseInt(false);
+console.info("parseInt(false)",num);
+var num = parseInt(undefined);
+console.info("parseInt(undefined)", num);
+var num =  parseInt(20,2)
+console.info(" parseInt(20,2)");
+console.info(num)
+var num =  parseInt(20,8)
+console.info(" parseInt(20,8)");
+console.info(num)
+var num =  parseInt(20,16)
+console.info(" parseInt(20,16)");
+console.info(num)
+
+/*
+	隐式转换
+*/
+var num = 1+'1';
+console.info("1+'1':",(1+'1'));
