37b96f42da62bb3da257d5d6a91e3702a9ee6200
hjj <1459638155@qq.com>
2020-03-09 13:32:44 +0800

    js

--- javascript/regular.js
+++ javascript/regular.js
@@ -24,10 +24,59 @@ var str = 'abcd'
 console.info(reg.test(str))
 /*
 i:ignoreCase(忽略大小写)
-g:全局
-m:
+g:全局匹配
+m:多行匹配
 */
 var reg1 = new RegExp("abc","g");
 console.info(reg1)
 console.info(reg1.test(str))
 console.info(str.match(reg))
+
+/*
+^首个元素
+[^]范围内非(除了)
+*/
+var str2 = 'abc\nabcda';
+var reg2 = /^a/gm;
+console.info(str2.match(reg2))
+
+
+/*
+[]:1位值 区间
+*/
+var reg3 = /[1-9][A-z][1-9]/g;
+var str3 = "123dasd323sfsd555";
+console.info(str3.match(reg3))
+
+/*
+（|）或这
+*/
+
+/*
+元字符
+	是拥有特殊含义的字符
+\w ===[0-9A-z_]
+\W[^\w]
+\d[0-9]
+\D[^0-9]
+\s [\t\n\r\v\f]空格字符
+\S[^\s]
+\b 单词边界
+\B 非单词编辑
+/\b33\B/
+*/
+
+/*
+unicode
+ 能包含上文字
+ 按照层去划分
+ unicode编码和汉字去进行匹配
+*/
+console.info(\u8001);
+// console.info(\u100000-\U10ffff);
+
+/*
+一切集合
+[\d/D]
+.[^|r|n]
+*/
